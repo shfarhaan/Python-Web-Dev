a24593254cfc044acc78ca00ed2eed6c34133da6
farhaan-sh <shf.cse@gmail.com>
2025-06-26 22:41:34 +0600

    hw2

--- Home Work/hw2.py
+++ Home Work/hw2.py
@@ -18,15 +18,12 @@ elif 74 <= marks <= 70:
     print("B-")
 elif 69 <= marks <= 65:
     print("C+")
-
-elif 84 <= marks <= 80:
-    print("B+")
-elif 79 <= marks <= 75:
-    print("B")
-elif 74 <= marks <= 70:
-    print("B-")
-elif 69 <= marks <= 65:
-    print("C+")
+elif 64 <= marks <= 60:
+    print("C")
+elif 59 <= marks <= 55:
+    print("C-")
+elif 54 <= marks <= 50:
+    print("D")
 else:
-    print("Temp range non-existant!")
+    print("F")
     
