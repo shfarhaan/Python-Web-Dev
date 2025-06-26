e0f5fdc1173eab755aa77f06ed00f76f6236bf91
farhaan-sh <shf.cse@gmail.com>
2025-06-26 22:35:58 +0600

    Innovative Skills - Django Batch 9

--- Home Work/hw2.py
+++ Home Work/hw2.py
@@ -2,3 +2,31 @@
 # user will input his marks:
 # Grade: A, A, B, C......
 
+marks = int(input("Please enter your marks: "))
+
+if marks <= 97 :
+    print("A+")
+elif 97 < marks <= 90:
+    print("A")
+elif 89 <= marks <= 85:
+    print("A-")
+elif 84 <= marks <= 80:
+    print("B+")
+elif 79 <= marks <= 75:
+    print("B")
+elif 74 <= marks <= 70:
+    print("B-")
+elif 69 <= marks <= 65:
+    print("C+")
+
+elif 84 <= marks <= 80:
+    print("B+")
+elif 79 <= marks <= 75:
+    print("B")
+elif 74 <= marks <= 70:
+    print("B-")
+elif 69 <= marks <= 65:
+    print("C+")
+else:
+    print("Temp range non-existant!")
+    
